bb499b54e1e8700d1ff1a846a40abedc11c57308
tumahung <hungtp.dalat@gmail.com>
2025-11-16 23:20:29 +0700

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -100,9 +100,9 @@ export const siteConfig: SiteConfig = {
     // 背景图片配置
     src: {
       // 桌面背景图片
-      desktop: "/assets/images/d1.webp",
+      desktop: "/assets/images/mainview-secret-1.jpg",
       // 移动背景图片
-      mobile: "/assets/images/m1.webp",
+      mobile: "/assets/images/mainview-secret-1.jpg",
     },
 
     // Banner模式特有配置
